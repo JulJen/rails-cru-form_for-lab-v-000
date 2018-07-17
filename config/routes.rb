d38a5b01e9532341de2643a31f85f2409f7e3d70
Jul Jen <juliajen.prog@gmail.com>
2018-07-17 05:17:48 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'song/create'
+
+  get 'song/update'
+
+  get 'song/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
